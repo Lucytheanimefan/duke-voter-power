80114f15671e0aa1cc5a2ccde94dc1bad82388a3
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-09-12 13:59:34 -0400

    Reformatted dictionary that contains voting info

--- scraper.py
+++ scraper.py
@@ -6,12 +6,64 @@
 '''
 import requests, bs4
 
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
 res=requests.get('http://projects.fivethirtyeight.com/2016-election-forecast/')
 res.raise_for_status
 
 soup=bs4.BeautifulSoup(res.text,'html.parser')
 
-def get_voting_info(class_name):
+def get_voting_info(class_name, info_type):
 	state_name = ""
 	num_value=None
 	json_dict={}
@@ -23,13 +75,16 @@ def get_voting_info(class_name):
 	i=0;
 	for state in roi_table_soup.find_all(['div','span'],{'class':['long','pct']}):
 		if i%2==0:
-			state_name=state.get_text().strip("<")
+			state_name=state.get_text().strip("<").rstrip()
 		else: 
-			num_value=state.get_text().strip("<")
+			num_value=state.get_text().strip("<").rstrip()
 
 		if state_name and num_value:
 			#print state_name +","+num_value
-			json_dict[state_name.encode('utf=8')]=num_value.encode('utf-8')
+			if state_name.encode('utf=8') in us_state_abbrev:
+				json_dict[us_state_abbrev[state_name.encode('utf=8')]]={info_type: num_value.encode('utf-8')}
+			else:
+				json_dict[state_name.encode('utf=8')]={info_type:num_value.encode('utf-8')}
 			#reset to none
 			state_name=None
 			num_value=None
@@ -38,11 +93,11 @@ def get_voting_info(class_name):
 		
 
 
-print get_voting_info('roi-table')
+print get_voting_info('roi-table','votingPowerIndex')
 
 print "--------------------------"
 
-print get_voting_info('tipping-table')
+print get_voting_info('tipping-table','tippingPower')
 
 
 #def format_data_json(data):
